b763d01ce75989db61ef3c665f5fc8be1b9e1054
liyc <1476519404@qq.com>
2022-09-17 10:57:57 +0800

    front first commit

---
+++ front/src/views/common/home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <img src="~@/assets/img/home_bg.png" width="100%" :height="height" />
+  </div>
+</template>``
+
+<script>
+  export default {
+    data () {
+      return {
+        height: this.$store.state.common.documentClientHeight + 'px'
+      }
+    },
+    created () {
+      this.height = '100%'
+    },
+    mounted () {
+      console.log(this.$store.state.common.documentClientHeight)
+    }
+  }
+</script>
+
